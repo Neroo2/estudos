6309c22757cd71afba09adb82de4411166eb786b
Nero <nelsonnnnn.junior@gmail.com>
2025-12-31 15:06:08 -0300

    Shopping Cart program added.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,3 @@
-import java.util.Scanner;
-
-
 void main() {
-    Scanner scanner = new Scanner(System.in);
-
-
-
-
-    scanner.close();
+    
 }
---
+++ src/ShoppingCart.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+void main() {
+
+    Scanner scan = new Scanner(System.in);
+
+    String item;
+    double price;
+    int quantity;
+    double total;
+
+    IO.println("Enter the item name: ");
+    item = scan.nextLine();
+
+    IO.println("Enter the item price: ");
+    price = scan.nextDouble();
+
+    IO.println("Enter the number of items: ");
+    quantity = scan.nextInt();
+
+    total = price * quantity;
+
+    System.out.printf("Your shopping cart total is: %.2f  and you chose the current item: %s ", total, item);
+
+}
